5be789e3df1298a936e5429802e01689e13ea101
Zweizack <101064153+Zweizack@users.noreply.github.com>
2022-03-07 23:59:20 +0300

    Update delta-illusion.go

--- delta-illusion.go
+++ delta-illusion.go
@@ -1,69 +1,69 @@
 package main
 
 import (
-	"bufio"
-	"crypto/md5"
-	"crypto/sha256"
-	"encoding/hex"
-	"fmt"
-	"os"
-	"os/exec"
-	"strings"
+  "bufio"
+  "crypto/md5"
+  "crypto/sha256"
+  "encoding/hex"
+  "fmt"
+  "os"
+  "os/exec"
+  "strings"
 
-	"github.com/fatih/color"
+  "github.com/fatih/color"
 )
 
 func Line() {
-	white := color.New(color.FgWhite, color.Bold)
-	white.Println(strings.Repeat("-", 42))
+  white := color.New(color.FgWhite, color.Bold)
+  white.Println(strings.Repeat("-", 42))
 }
 
 func Input(name string) string {
-	green := color.New(color.FgGreen, color.Bold)
-	green.Print(name)
+  green := color.New(color.FgGreen, color.Bold)
+  green.Print(name)
 
-	color.Set(color.FgYellow, color.Bold)
-	defer color.Unset()
+  color.Set(color.FgYellow, color.Bold)
+  defer color.Unset()
 
-	input := bufio.NewScanner(os.Stdin)
-	input.Scan()
+  input := bufio.NewScanner(os.Stdin)
+  input.Scan()
 
-	return input.Text()
+  return input.Text()
 }
 
 func Encryption(data string) string {
-	// sha256(data) result
-	s256Hash := sha256.Sum256([]byte(data))
-	s256Text := hex.EncodeToString(s256Hash[:])
-	// md5(sha256(data)) result
-	wandHash := md5.Sum([]byte(s256Text))
-	wandText := hex.EncodeToString(wandHash[:])
+  // sha256(data) result
+  s256Hash := sha256.Sum256([]byte(data))
+  s256Text := hex.EncodeToString(s256Hash[:])
+  // md5(sha256(data)) result
+  wandHash := md5.Sum([]byte(s256Text))
+  wandText := hex.EncodeToString(wandHash[:])
 
-	return wandText
+  return wandText
 }
 
 func main() {
 
-	Execute, _ := exec.Command("sh", "-c", "clear").Output()
-	fmt.Print(string(Execute))
+  Execute, _ := exec.Command("sh", "-c", "clear").Output()
+  fmt.Print(string(Execute))
 
-	green := color.New(color.FgGreen, color.Bold).SprintFunc()
-	yellow := color.New(color.FgYellow, color.Bold).SprintFunc()
+  green := color.New(color.FgGreen, color.Bold).SprintFunc()
+  yellow := color.New(color.FgYellow, color.Bold).SprintFunc()
 
-	Line()
+  Line()
 
-	color.Set(color.FgYellow, color.Bold)
-	defer color.Unset()
+  color.Set(color.FgYellow, color.Bold)
+  defer color.Unset()
 
-	δ := Input("δpq: ")
-	Δ := Input("Δpq: ")
+  δ := Input("δpq: ")
+  Δ := Input("Δpq: ")
 
-	Line()
+  Line()
 
-	for i := 0; i < 64; i++ {
-		δ = Encryption(δ)
-		fmt.Printf("[%v]:\t%v\n", green(i), yellow(δ[:0x11], Δ))
-	}
+  for i := 0; i < 64; i++ {
+    δ = Encryption(δ)
+    fmt.Printf("[%v]:\t%v\n", green(i), yellow(δ[:0x11], Δ))
+  }
 
-	Line()
+  Line()
 }
